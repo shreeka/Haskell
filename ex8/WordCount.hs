6d7b3e4b26c7b5ba03e4679630885d61e06732d3
Shreeka Shakya <shreeka.shakya@gmail.com>
2019-06-26 09:51:57 +0200

    Ex8 Solutions

--- ex8/WordCount.hs
+++ ex8/WordCount.hs
@@ -4,7 +4,18 @@ module Main
 where
 import System.Environment
 
+{--showln  = (++ "\n") . show
+
+wc_l inp  = showln(length(lines inp))
+wc_w inp = showln(length(words inp))
+wc_c inp = showln(length(inp))--}
+
 main :: IO ()
 main = do
     args <- getArgs
-    putStrLn "To be done..."
+    putStrLn "To be done..."--
+
+{--main = interact wc
+    where wc input =  wc_l input ++ wc_w input ++ wc_c input--}
+
+-- We couldn't figure out a way to apply with args,hence we used the help of interact function.
